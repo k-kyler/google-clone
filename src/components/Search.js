8444f160a4ac989a6b875eea0834261b4054e8de
K-Kyler <khaiquang690@gmail.com>
2021-01-10 16:34:23 +0700

    Add home page UI, configure search input

---
+++ src/components/Search.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./Search.css";
+import SearchIcon from "@material-ui/icons/Search";
+import KeyboardIcon from "@material-ui/icons/Keyboard";
+import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
+function Search({ hideButtons = false }) {
+    const [input, setInput] = useState("");
+    const history = useHistory();
+
+    const searchHandler = (e) => {
+        e.preventDefault();
+
+        history.push("/search");
+    };
+
+    return (
+        <form className="search">
+            <div className="search__input">
+                <SearchIcon className="search__inputIcon" />
+                <input
+                    onChange={(e) => setInput(e.target.value)}
+                    value={input}
+                />
+                <KeyboardIcon className="search__keyboardIcon" />
+                <MicIcon />
+            </div>
+
+            {!hideButtons ? (
+                <div className="search__buttons">
+                    <Button
+                        type="submit"
+                        onClick={searchHandler}
+                        variant="outlined"
+                    >
+                        Google Search
+                    </Button>
+                    <Button variant="outlined">I'm Feeling Lucky</Button>
+                </div>
+            ) : (
+                <div className="search__buttons">
+                    <Button
+                        className="search__buttonsHidden"
+                        type="submit"
+                        onClick={searchHandler}
+                        variant="outlined"
+                    >
+                        Google Search
+                    </Button>
+                    <Button
+                        className="search__buttonsHidden"
+                        variant="outlined"
+                    >
+                        I'm Feeling Lucky
+                    </Button>
+                </div>
+            )}
+        </form>
+    );
+}
+
+export default Search;
